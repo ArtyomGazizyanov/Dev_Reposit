37ee8902c8e79eb5b05767bc935d9835cd97a4ca
ArtyomGazizyanov <Artyomgad@gmail.com>
2016-11-12 13:11:17 +0300

    переделал перерасчет линии дамок на игровом поле

--- Сheckers/ChangeCheckers.cpp
+++ Сheckers/ChangeCheckers.cpp
@@ -1,7 +1,26 @@
 #include "Header.h"
+  
+std::vector <int> QUEEN_LINE_FOR_WHITE; 
+std::vector <int> QUEEN_LINE_FOR_BLACK;
 
-std::vector <int> QUEEN_LINE_FOR_WHITE = { 1, 3, 5, 7 , 17};
-std::vector <int> QUEEN_LINE_FOR_BLACK = { 56, 58, 60, 62 };
+void FindQueenLines(BoardStruct &board)//, std::vector <int> &QUEEN_LINE_FOR_WHITE, std::vector <int> &QUEEN_LINE_FOR_BLACK)
+{
+	for (int i = 0; i < BOARD_SIZE; ++i)
+	{
+		if (board.cells[i].isEnableToPlay)
+		{
+			QUEEN_LINE_FOR_WHITE.push_back(i);
+		}
+	}
+
+	for (int i = TOTAL_AMOUNT_OF_CELLS - BOARD_SIZE; i < TOTAL_AMOUNT_OF_CELLS; ++i)
+	{
+		if (board.cells[i].isEnableToPlay)
+		{
+			QUEEN_LINE_FOR_BLACK.push_back(i);
+		}
+	}
+}
 
 sf::Vector2f GrabTheChecker(BoardStruct &board, int currChecker, int &whichCheckerCanMove, sf::Vector2f positionOfCursor,
 					std::vector <CheckerWay> &possibleWays)
@@ -26,7 +45,7 @@ void SetOriginCheckers(BoardStruct &board)
 		}
 	}
 
-	for (int i = 40; i < TOTAL_AMOUNT_OF_CELLS; i++)
+	for (int i = TOTAL_AMOUNT_OF_CELLS - 24; i < TOTAL_AMOUNT_OF_CELLS && board.amountOfWhiteCheckers < 12; i++)
 	{
 		if (board.cells[i].isEnableToPlay)
 		{
@@ -36,33 +55,35 @@ void SetOriginCheckers(BoardStruct &board)
 		}
 	}
 
-	int currentChecker = 0;
+	Checker *CurChecker;
+	int currentCheckerIndex = 0;
 	for (int row = 0; row < BOARD_SIZE; row++)
 	{
 		for (int col = 0; col < BOARD_SIZE; col++)
 		{
-			currentChecker = BOARD_SIZE * row + col;
-			if (board.cells[currentChecker].checker.color == white)
+			currentCheckerIndex = BOARD_SIZE * row + col;
+			CurChecker = &board.cells[currentCheckerIndex].checker;
+			if (CurChecker->color == white)
 			{ 
-				board.cells[currentChecker].checker.col = col;
-				board.cells[currentChecker].checker.row = row;
-				board.cells[currentChecker].checker.shape = sf::CircleShape(CHECKERS_SIZE);
-				board.cells[currentChecker].checker.shape.setOutlineThickness(1);
-				board.cells[currentChecker].checker.shape.setOutlineColor(sf::Color::Black);
-				board.cells[currentChecker].checker.shape.setFillColor(sf::Color::White);
-				board.cells[currentChecker].checker.shape.setPosition((col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2, (row + 1)* SQUARE_SIZE + CHECKERS_SIZE / 2);
+				CurChecker->col = col;
+				CurChecker->row = row;
+				CurChecker->shape = sf::CircleShape(CHECKERS_SIZE);
+				CurChecker->shape.setOutlineThickness(1);
+				CurChecker->shape.setOutlineColor(sf::Color::Black);
+				CurChecker->shape.setFillColor(sf::Color::White);
+				CurChecker->shape.setPosition((col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2, (row + 1)* SQUARE_SIZE + CHECKERS_SIZE / 2);
 			}
-			else
-				if (board.cells[currentChecker].checker.color == black)
-				{ 
-					board.cells[currentChecker].checker.col = col;
-					board.cells[currentChecker].checker.row = row;
-					board.cells[currentChecker].checker.shape = sf::CircleShape(CHECKERS_SIZE);
-					board.cells[currentChecker].checker.shape.setOutlineThickness(1);
-					board.cells[currentChecker].checker.shape.setOutlineColor(sf::Color::White);
-					board.cells[currentChecker].checker.shape.setFillColor(sf::Color::Black);
-					board.cells[currentChecker].checker.shape.setPosition((col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2, (row + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2);
-				}
+			else if (CurChecker->color == black)
+			{ 
+				CurChecker->col = col;
+				CurChecker->row = row;
+				CurChecker->shape = sf::CircleShape(CHECKERS_SIZE);
+				CurChecker->shape.setOutlineThickness(1);
+				CurChecker->shape.setOutlineColor(sf::Color::White);
+				CurChecker->shape.setFillColor(sf::Color::Black);
+				CurChecker->shape.setPosition((col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2, (row + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2);
+			}
+			
 		}
 	}
 }
@@ -84,25 +105,27 @@ void SetOriginSettingsOfChecker(Checker &checker)
 
 void BuildBackground(BoardStruct &board)
 {
-	int i = 0;
+	MySquare *currCells;
 	for (int row = 1; row <= BOARD_SIZE; row++)
 	{
 		for (int col = 1; col <= BOARD_SIZE; col++)
-		{ 
-			board.cells[i].col = col-1;
-			board.cells[i].row = row-1;
-			board.cells[i].square.setSize(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
-			board.cells[i].square.setPosition(col * SQUARE_SIZE, row * SQUARE_SIZE);
-			board.cells[i].square.setOutlineColor(sf::Color::Black);
-			board.cells[i].square.setOutlineThickness(OUTBOARD_WIDTH);
+		{
+			const int i = (row - 1) * BOARD_SIZE + (col - 1);
+			currCells = &board.cells[i];
+			currCells->col = col-1;
+			currCells->row = row-1;
+			currCells->square.setSize(sf::Vector2f(SQUARE_SIZE, SQUARE_SIZE));
+			currCells->square.setPosition(col * SQUARE_SIZE, row * SQUARE_SIZE);
+			currCells->square.setOutlineColor(sf::Color::Black);
+			currCells->square.setOutlineThickness(OUTBOARD_WIDTH);
 			if ((col + row + 1) % 2 == 0)
 			{
-				board.cells[i].square.setFillColor(GREY);
-				board.cells[i].isEnableToPlay = true;
+				currCells->square.setFillColor(GREY);
+				currCells->isEnableToPlay = true;
 			}
-			i++;
 		}
 	}
+	FindQueenLines(board);
 }
  
 
@@ -145,54 +168,51 @@ void MakeEmptySquare(MySquare &cell)
 int FindKilledCheckerNumber(int const &newCheckerNum, int const &whichChecker)
 {
 	int deletedCheckerNum = 0; 
-	if ((newCheckerNum - whichChecker) == 18)
+	if ((newCheckerNum - whichChecker) == DIAGONAL_RIGHT_CELL_NUM * 2)
 	{
-		deletedCheckerNum = whichChecker + 9; 
+		deletedCheckerNum = whichChecker + DIAGONAL_RIGHT_CELL_NUM;
 	}
-	else
-		if ((newCheckerNum - whichChecker) == -18)
-		{
-			deletedCheckerNum = whichChecker - 9; 
-		}
-		else
-			if ((newCheckerNum - whichChecker) == 14)
-			{
-				deletedCheckerNum = whichChecker + 7; 
-			}
-			else
-				if ((newCheckerNum - whichChecker) == -14)
-				{
-					deletedCheckerNum = whichChecker - 7;  
-				}
+	else if ((newCheckerNum - whichChecker) == -DIAGONAL_RIGHT_CELL_NUM * 2)
+	{
+		deletedCheckerNum = whichChecker - DIAGONAL_RIGHT_CELL_NUM;
+	}
+	else if ((newCheckerNum - whichChecker) == DIAGONAL_LEFT_CELL_NUM * 2)
+	{
+		deletedCheckerNum = whichChecker + DIAGONAL_LEFT_CELL_NUM;
+	}
+	else if ((newCheckerNum - whichChecker) == -DIAGONAL_LEFT_CELL_NUM * 2)
+	{
+		deletedCheckerNum = whichChecker - DIAGONAL_LEFT_CELL_NUM;
+	}
 	return deletedCheckerNum;
 }
 
 std::vector <int> FintFindKilledCheckerNumber_Queen(BoardStruct board, int newCheckerNum, int whichChecker)
 { 
-	std::vector <int> CheckersToDie;
+	std::vector <int> checkersToDie;
 	int deltaCheckerNum = 0; 
-	if ((newCheckerNum - whichChecker) % 9 == 0)
+	if ((newCheckerNum - whichChecker) % DIAGONAL_RIGHT_CELL_NUM == 0)
 	{
 		if (newCheckerNum < whichChecker)
 		{
-				deltaCheckerNum = -9;
+				deltaCheckerNum = -DIAGONAL_RIGHT_CELL_NUM;
 		}
 		else
 		{
-			deltaCheckerNum = 9;
+			deltaCheckerNum = DIAGONAL_RIGHT_CELL_NUM;
 		}
 		 
 	}
 	else
-			if ((newCheckerNum - whichChecker) % 7 == 0)
+			if ((newCheckerNum - whichChecker) % DIAGONAL_LEFT_CELL_NUM == 0)
 			{
 				if (newCheckerNum < whichChecker)
 				{
-					deltaCheckerNum = -7;
+					deltaCheckerNum = -DIAGONAL_LEFT_CELL_NUM;
 				}
 				else
 				{
-					deltaCheckerNum = 7;
+					deltaCheckerNum = DIAGONAL_LEFT_CELL_NUM;
 				} 
 			}
 	int currChecker = whichChecker + deltaCheckerNum; 
@@ -200,12 +220,12 @@ std::vector <int> FintFindKilledCheckerNumber_Queen(BoardStruct board, int newCh
 	{ 
 		if (!board.cells[currChecker].isEmpty)
 		{
-			CheckersToDie.push_back(currChecker);
+			checkersToDie.push_back(currChecker);
 		}
 		currChecker += deltaCheckerNum; 
 	} 
 
-	return	CheckersToDie;
+	return	checkersToDie;
 }
 
 void DeleteKilledChecker(BoardStruct &board, int const &newCheckerNum, int const &whichChecker)
@@ -300,10 +320,11 @@ bool isCheckerMoving(BoardStruct &board, int &whichCheckerCanMove, CheckerColor 
 	std::vector <CheckerWay> &possibleWays, sf::Vector2f &deltaPosition)
 {
 	bool isMove = false;
+	Checker  checkerToKill = board.cells[killingChecker].checker;
 	if (killingChecker != 0)
 	{
-		if (board.cells[killingChecker].checker.color == checkerColor 
-			&& board.cells[killingChecker].checker.shape.getGlobalBounds().contains(positionOfCursor.x, positionOfCursor.y) 
+		if (checkerToKill.color == checkerColor
+			&& checkerToKill.shape.getGlobalBounds().contains(positionOfCursor.x, positionOfCursor.y)
 			&& (board.cells[killingChecker].isEnableToPlay))//è ïðè ýòîì êîîðäèíàòà êóðñîðà ïîïàäàåò â ñïðàéò
 		{
 			deltaPosition = GrabTheChecker(board, killingChecker, whichCheckerCanMove, positionOfCursor, possibleWays);
@@ -324,18 +345,19 @@ bool isCheckerMoving(BoardStruct &board, int &whichCheckerCanMove, CheckerColor 
 }
  
 void IfWrongWayCheckerGoBackToStartPostion(BoardStruct &board, int whichChecker, int newCheckerNum, std::vector <CheckerWay> possibleWay)
-{
+{ 
+	Checker *ñhoosenChecker = &board.cells[whichChecker].checker;
 	if (whichChecker == newCheckerNum || board.cells[newCheckerNum].checker.color != none)
 	{
-		board.cells[whichChecker].checker.shape.setPosition((board.cells[whichChecker].checker.col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2,
-			(board.cells[whichChecker].checker.row + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2);
+		ñhoosenChecker->shape.setPosition((ñhoosenChecker->col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2,
+			(ñhoosenChecker->row + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2);
 	}
 	for (int i = 0; i < possibleWay.size(); i++)
 	{
 		if (newCheckerNum != possibleWay[i].CheckerNum)
 		{
-			board.cells[whichChecker].checker.shape.setPosition((board.cells[whichChecker].checker.col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2,
-				(board.cells[whichChecker].checker.row + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2);
+			ñhoosenChecker->shape.setPosition((ñhoosenChecker->col + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2,
+				(ñhoosenChecker->row + 1) * SQUARE_SIZE + CHECKERS_SIZE / 2);
 		}
 	}
 }
@@ -354,13 +376,13 @@ void CheckAnyKillerWays(std::vector <CheckerWay> possibleWay, bool &isAnyKillerW
 
 ChckerMovement SetChoosenCheckerToThePosition(BoardStruct &board, int whichChecker, sf::Vector2f position, std::vector <CheckerWay> possibleWay)
 {
-	int newCol = position.x - 1;
-	int newRow = position.y - 1;
+	auto newCol = position.x - 1;
+	auto newRow = position.y - 1;
 	Checker newChecker; 
 	ChckerMovement isCheckerMoved;
 	isCheckerMoved = no;
 	bool isAnyKillerWays = false;
-	int newCheckerNum = (newRow)* BOARD_SIZE + (newCol);
+	int newCheckerNum = static_cast<int>((newRow)* BOARD_SIZE + (newCol));
 	CheckAnyKillerWays(possibleWay, isAnyKillerWays);
 
 	if (board.cells[newCheckerNum].isEmpty)
@@ -412,9 +434,12 @@ ChckerMovement SetChoosenCheckerToThePosition(BoardStruct &board, int whichCheck
 
 bool  SetCheckerOnBoard(BoardStruct &board, int whichCheckerCanMove, sf::Vector2f positionOfCursor, std::vector <CheckerWay> &possibleWays, int &killingChecker, bool &isWhitePlayerTurn)
 {
-	int col = positionOfCursor.x / SQUARE_SIZE;
-	int row = positionOfCursor.y / SQUARE_SIZE;
-	sf::Vector2f Coordinate(col, row);
+	int col = static_cast<int>(positionOfCursor.x / SQUARE_SIZE);
+	int row = static_cast<int>(positionOfCursor.y / SQUARE_SIZE);
+	float fcol = static_cast<float>(col);
+	float frow = static_cast<float>(row);
+
+	sf::Vector2f  Coordinate(fcol, frow);
 	bool isSetChecker = false;
 	ChckerMovement checkerMovement = SetChoosenCheckerToThePosition(board, whichCheckerCanMove, Coordinate, possibleWays);
 	if (checkerMovement == setUp)
@@ -427,7 +452,7 @@ bool  SetCheckerOnBoard(BoardStruct &board, int whichCheckerCanMove, sf::Vector2
 	{
 		if (checkerMovement == kill)
 		{
-			killingChecker = Coordinate.x - 1 + (Coordinate.y - 1) * BOARD_SIZE;
+			killingChecker = (Coordinate.x - 1 + (Coordinate.y - 1) * BOARD_SIZE);
 		}
 	}
 
